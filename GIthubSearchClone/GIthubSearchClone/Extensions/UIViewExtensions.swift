b8749315ac9996c20b1f397831bc878bfc25e75c
jun7680 <injunock@naver.com>
2023-02-23 21:57:05 +0900

    Add BaseTableViewCell, UIViewExtension, UIStackViewExtension

---
+++ GIthubSearchClone/GIthubSearchClone/Extensions/UIViewExtensions.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewExtensions.swift
+//  GIthubSearchClone
+//
+//  Created by 옥인준 on 2023/02/23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    /// Add Subview
+    /// - Parameter views: [UIView]
+    func addSubViews(_ views: [UIView]) {
+        views.forEach {
+            addSubview($0)
+        }
+    }
+    
+}
+
+extension UIStackView {
+    
+    /// ArrangeSubView for UIStackView
+    /// - Parameter child: [UIView]
+    func addArrangeSubViews(_ child: [UIView]) {
+        child.forEach {
+            addArrangedSubview($0)
+        }
+    }
+}
---
+++ GIthubSearchClone/GIthubSearchClone/ViewControllers/Base/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  GIthubSearchClone
+//
+//  Created by 옥인준 on 2023/02/23.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {}
+    func setupViews() {}
+    func initConstraints() {}
+}
