f06f84cfa8107f67848159dde02b7d0396b548ea
jun7680 <injunock@naver.com>
2023-02-25 00:57:21 +0900

    Add ViewModel(OptionViewModel, SearchListViewModel), UpdateViewModel(SearchMainViewMode)

--- GIthubSearchClone/GIthubSearchClone/ViewModel/SearchMainViewModel.swift
+++ GIthubSearchClone/GIthubSearchClone/ViewModel/SearchMainViewModel.swift
@@ -12,26 +12,25 @@ import RxRelay
 protocol SearchMainViewModelInput {
     func search(term: String, sort: SortType, order: OrderType)
     
-    var searchListRelay: PublishRelay<[SearchDTO]> { get }
+    var searchParamsRelay: PublishRelay<SearchParameters> { get }
 }
 
 protocol SearchMainViewModelOutput {
-    var searchListObservable: Observable<[SearchDTO]> { get }
+    var searchParamsObservable: Observable<SearchParameters> { get }
 }
 
 class SearchMainViewModel: SearchMainViewModelInput,
                            SearchMainViewModelOutput {
     
     private let disposeBag = DisposeBag()
-    private var searchListItems = [SearchDTO]()
     
     var inputs: SearchMainViewModelInput { return self }
     var outputs: SearchMainViewModelOutput { return self }
     
     // List relay property
-    var searchListRelay = PublishRelay<[SearchDTO]>()
-    var searchListObservable: Observable<[SearchDTO]> {
-        return searchListRelay.asObservable()
+    var searchParamsRelay = PublishRelay<SearchParameters>()
+    var searchParamsObservable: Observable<SearchParameters> {
+        return searchParamsRelay.asObservable()
     }
     
     func search(term: String, sort: SortType, order: OrderType) {
@@ -39,17 +38,9 @@ class SearchMainViewModel: SearchMainViewModelInput,
             term: term,
             sort: sort,
             order: order,
-            limit: 30,
+            limit: 20,
             page: 1
         )
-        
-        SearchService.fetch(params: searchParam)
-            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
-            .subscribe(with: self) { owner, result in
-                print(result)
-                owner.searchListItems.append(contentsOf: result.items)
-                owner.searchListRelay.accept(owner.searchListItems)
-            }.disposed(by: disposeBag)
+        searchParamsRelay.accept(searchParam)
     }
-
 }
